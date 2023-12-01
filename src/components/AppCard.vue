f535186ab5cb5de7cc786e661adc5e13454a6fdb
Luca Gregori <luca.gregori98@gmail.com>
2023-12-01 13:10:12 +0100

    Add: poster in card

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -5,7 +5,7 @@ export default {
     },
     data() {
         return {
-            flags: ["en" , "it" , "es", "fr"],
+            flags: ["en" , "it" , "es", "fr","ru"],
         };
     },
     methods: {
@@ -13,11 +13,19 @@ export default {
             //console.log(img);
             return new URL(`../assets/img/${img}.png`, import.meta.url).href;
         },
+    },
+    computed: {
+        vote() {
+            return Math.ceil(parseInt(this.movieObj.vote_average) / 2);
+        }
     }
 };
 </script>
 <template>
 <div class="card">
+    <div class="poster">
+        <img :src="`https://image.tmdb.org/t/p/w342/${movieObj.poster_path}`" alt="">
+    </div>
         <h2>{{ movieObj.title ? movieObj.title : movieObj.name }}</h2>
         <h3>{{ movieObj.original_title ? movieObj.original_title : movieObj.original.name }}</h3>
         <img class="flag" :src="getImagePath(movieObj.original_language)" alt="" v-if="flags.includes(movieObj.original_language)">
